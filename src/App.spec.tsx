df16913820d8054f7636aa94a6fc16c40c9e2206
Nathan Arthur <nathan@nathanarthur.com>
2022-12-20 11:17:38 -0500

    display countdown

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 describe("App", () => {
   it("has task input", () => {
@@ -20,10 +21,33 @@ describe("App", () => {
 
     expect(screen.getByRole("button")).toBeInTheDocument();
   });
+
+  it("has countdown", async () => {
+    render(<App />);
+
+    const user = userEvent.setup();
+
+    await user.type(screen.getByLabelText("Task"), "test");
+    await user.clear(screen.getByLabelText("Time"));
+    await user.type(screen.getByLabelText("Time"), "00:00");
+    await user.click(screen.getByRole("button"));
+
+    expect(await screen.findByText("00:00")).toBeInTheDocument();
+  });
+
+  it("uses given time for countdown", async () => {
+    render(<App />);
+
+    const user = userEvent.setup();
+
+    await user.type(screen.getByLabelText("Task"), "test");
+    await user.clear(screen.getByLabelText("Time"));
+    await user.type(screen.getByLabelText("Time"), "00:01");
+    await user.click(screen.getByRole("button"));
+
+    expect(await screen.findByText("00:01")).toBeInTheDocument();
+  });
 });
 
 // TODO
-// task input
-// time input
-// start button
 // countdown
--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,23 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 function App() {
+  const [task, setTask] = useState("");
+  const [time, setTime] = useState("00:00");
+
   return (
     <div className="App">
       <TextField id="task" label="Task" variant="outlined" />
-      <TextField id="time" label="Time" variant="outlined" />
+      <TextField
+        id="time"
+        label="Time"
+        variant="outlined"
+        value={time}
+        onChange={(e) => setTime(e.target.value)}
+      />
       <Button variant="contained" onClick={() => {}}>
         Start
       </Button>
+      <div>{time}</div>
     </div>
   );
 }
