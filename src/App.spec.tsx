5cee5ca5627bb1ef290b8946f14c82b72dbdc0e5
Nathan Arthur <nathan@nathanarthur.com>
2022-12-20 15:37:15 -0500

    add estimation page

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,7 @@
 import { describe, it, expect, beforeEach, afterEach, vi } from "vitest";
 import { render, screen, act, waitFor } from "@testing-library/react";
 import App from "./App";
+import userEvent from "@testing-library/user-event";
 
 vi.mock("./lib/speak");
 
@@ -19,5 +20,47 @@ describe("App", () => {
     render(<App />);
 
     expect(await screen.findByRole("textbox")).toBeInTheDocument();
+  });
+
+  it("has submit button", async () => {
+    render(<App />);
+
+    expect(
+      await screen.findByRole("button", { name: /submit/i })
+    ).toBeInTheDocument();
+  });
+
+  it("asks for time estimate for first task", async () => {
+    render(<App />);
+
+    userEvent.setup();
+
+    await userEvent.type(screen.getByRole("textbox"), "test");
+    await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+    expect(
+      await screen.findByText(/How long will it take/)
+    ).toBeInTheDocument();
+  });
+
+  it("does not ask for estimate before clicking submit", async () => {
+    render(<App />);
+
+    userEvent.setup();
+
+    await userEvent.type(screen.getByRole("textbox"), "test");
+
+    expect(screen.queryByText(/How long will it take/)).not.toBeInTheDocument();
+  });
+
+  it("displays text of task to estimate", async () => {
+    render(<App />);
+
+    userEvent.setup();
+
+    await userEvent.type(screen.getByRole("textbox"), "test");
+    await userEvent.click(screen.getByRole("button", { name: /submit/i }));
+
+    expect(await screen.findByText(/test/)).toBeInTheDocument();
   });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,78 @@ import Countdown from "./components/Countdown";
 import TextField from "@mui/material/TextField";
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import { useReducer } from "react";
+
+type Mode = "dump" | "estimate" | "countdown";
+type Action =
+  | { type: "setTasks"; payload: string }
+  | { type: "setMode"; payload: Mode };
+type State = { tasks: string[]; currentTask: number; mode: Mode };
+
+const initialState: State = {
+  tasks: [],
+  currentTask: 0,
+  mode: "dump",
+};
+
+function reducer(state: State, action: Action): State {
+  switch (action.type) {
+    case "setTasks":
+      return {
+        ...state,
+        tasks: action.payload.split("\n"),
+      };
+    case "setMode":
+      return {
+        ...state,
+        mode: action.payload,
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
     <CssBaseline>
       <Container maxWidth="sm">
-        <p>What would you like to work on today? Include one task per line.</p>
-        <TextField
-          id="tasks"
-          label="Tasks"
-          variant="outlined"
-          multiline
-          fullWidth
-        />
+        {state.mode === "dump" && (
+          <>
+            <p>
+              What would you like to work on today? Include one task per line.
+            </p>
+            <TextField
+              id="tasks"
+              label="Tasks"
+              variant="outlined"
+              multiline
+              fullWidth
+              value={state.tasks.join("\n")}
+              onChange={(e) =>
+                dispatch({ type: "setTasks", payload: e.target.value })
+              }
+            />
+            <Button
+              variant="contained"
+              onClick={() => {
+                dispatch({ type: "setMode", payload: "estimate" });
+              }}
+            >
+              Submit
+            </Button>
+          </>
+        )}
+
+        {state.mode === "estimate" && (
+          <>
+            <p>How long will it take?</p>
+            <p>"{state.tasks[0]}"</p>
+          </>
+        )}
+
         {/* <Countdown /> */}
       </Container>
     </CssBaseline>
