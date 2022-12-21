ba6a2094553d0d764b0445f6c12c7eeab3492bcd
Nathan Arthur <nathan@nathanarthur.com>
2022-12-21 09:32:32 -0500

    extract flow reducer

--- src/components/Flow.tsx
+++ src/components/Flow.tsx
@@ -1,77 +1,12 @@
 import Countdown from "./Countdown";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import { useReducer } from "react";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
-
-type Mode = "dump" | "estimate" | "countdown" | "review";
-type Action =
-  | { type: "setTasks"; payload: string }
-  | { type: "setMode"; payload: Mode }
-  | { type: "setSessionLength"; payload: string }
-  | { type: "completeTask" }
-  | { type: "nextTask" };
-type State = {
-  tasks: string[];
-  currentTask: number;
-  sessionLength: string;
-  mode: Mode;
-};
-
-const initialState: State = {
-  tasks: [],
-  currentTask: 0,
-  sessionLength: "",
-  mode: "dump",
-};
-
-function reducer(state: State, action: Action): State {
-  switch (action.type) {
-    case "setTasks":
-      return {
-        ...state,
-        tasks: action.payload.split("\n"),
-      };
-    case "setMode":
-      return {
-        ...state,
-        mode: action.payload,
-      };
-    case "setSessionLength":
-      return {
-        ...state,
-        sessionLength: action.payload,
-      };
-    case "completeTask":
-      // Define a new list of tasks that excludes the current task.
-      const tasks = state.tasks.filter(
-        (_, index) => index !== state.currentTask
-      );
-
-      const nextMode = tasks.length === 0 ? "dump" : "estimate";
-
-      return {
-        ...state,
-        tasks,
-        mode: nextMode,
-      };
-    case "nextTask":
-      const isLastTask = state.currentTask === state.tasks.length - 1;
-      const index = isLastTask ? 0 : state.currentTask + 1;
-
-      return {
-        ...state,
-        currentTask: index,
-        mode: state.tasks.length < 2 ? "dump" : "estimate",
-      };
-    default:
-      return state;
-  }
-}
+import useFlowReducer from "./Flow.reducer";
 
 function Flow() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [state, dispatch] = useFlowReducer();
 
   return (
     <>
