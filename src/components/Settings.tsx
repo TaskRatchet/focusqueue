8ec50c2fff72f0be19aec4e4275e7af3bf8f4cbb
Nathan Arthur <nathan@nathanarthur.com>
2023-02-10 16:57:25 -0500

    Display beeminder username

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,6 +1,6 @@
 import react from "react";
-import { Button } from "@mui/material";
-import { updateMe } from "../lib/firebase/firestore";
+import { Button, Input } from "@mui/material";
+import { updateMe, useMe } from "../lib/firebase/firestore";
 
 const redirectUri = "http://localhost:5173/settings";
 const clientId = "3yyb3d0ywheam1gjui40gn4eh";
@@ -16,5 +16,15 @@ if (token && username) {
 }
 
 export default function Settings() {
-  return <Button href={url}>Authorize app with Beeminder</Button>;
+  const me = useMe();
+
+  return me?.beeminderUsername ? (
+    <>Beeminder User: {me.beeminderUsername}</>
+  ) : (
+    <Button href={url}>Authorize app with Beeminder</Button>
+  );
 }
+
+// Show authenticated Beeminder username
+// Show an input to allow user to specify goalname
+// Store goalname in firestore
