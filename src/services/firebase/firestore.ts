8b8beae6d5d2569b8ecc2d2e29296340f51efb08
Nathan Arthur <nathan@nathanarthur.com>
2023-02-22 16:55:41 -0500

    Resolve type errors in firestore

--- src/services/firebase/firestore.ts
+++ src/services/firebase/firestore.ts
@@ -5,13 +5,17 @@ import {
   getDoc,
   updateDoc,
   onSnapshot,
+  UpdateData,
 } from "firebase/firestore";
 import app from "./app";
 import { useEffect, useMemo, useState } from "react";
 
-export function updateMe(data: unknown) {
+export function updateMe(data: UpdateData<unknown>) {
   const auth = getAuth(app);
   const user = auth.currentUser;
+  if (!user) {
+    throw new Error("updateMe: user is not authenticated");
+  }
   const firestore = getFirestore(app);
   const docRef = doc(firestore, "users", user.uid);
 
@@ -21,6 +25,10 @@ export function updateMe(data: unknown) {
 export function useMe(): undefined | Record<string, unknown> {
   const auth = getAuth(app);
   const user = auth.currentUser;
+  if (!user) {
+    throw new Error("useMe: user is not authenticated");
+  }
+
   const firestore = getFirestore(app);
   const docRef = useMemo(() => doc(firestore, "users", user.uid), [user]);
   const [me, setMe] = useState<Record<string, unknown>>();
