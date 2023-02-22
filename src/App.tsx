05f2c9f48a42417fb81562160cd179c84e377c6f
Nathan Arthur <nathan@nathanarthur.com>
2023-02-22 16:50:51 -0500

    Update imports to use aliases

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import CssBaseline from "@mui/material/CssBaseline";
 import Container from "@mui/material/Container";
-import Flow from "./components/Flow";
+import Flow from "@components/Flow";
 import { useAuthenticatedUser } from "@services/firebase/auth";
-import AuthControls from "./components/AuthControls";
+import AuthControls from "@components/AuthControls";
 import { Action, initialState, reducer, State } from "./App.reducer";
 import { createContext, Dispatch, useReducer } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -10,7 +10,7 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Settings from "./components/Settings";
+import Settings from "@components/Settings";
 
 const queryClient = new QueryClient();
 
